1a5bd180d0e77bba634acf4dd1a5eed8855581e5
Julia Lundstrum <julialundstrum@gmail.com>
2021-12-03 00:17:47 -0600

    Adding Education Section

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,24 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-    
+
     $("#thecarousel").carousel({
       interval: 2500
     });
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
 
 });
